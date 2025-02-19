abc32cd7835031de4178109c44c8b776b1c119fc
Carlos-Cao <carlos.cao2@gmail.com>
2025-02-19 23:11:27 +1300

    feat: add BookList component to fetch books

--- booklookup/src/app/page.tsx
+++ booklookup/src/app/page.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import BookList from "../components/BookList";
+
 export default function Home() {
   return (
     <div>
       <h1>BookLookUp</h1>
+      <BookList />
     </div>
   );
 }
